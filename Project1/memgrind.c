69dec77211fd037db3da207539cfec8d1f1a1058
Andrew <42828962+Lepieu@users.noreply.github.com>
2025-02-26 15:42:34 -0500

    Add files via upload

--- Project1/memgrind.c
+++ Project1/memgrind.c
@@ -1,70 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/time.h> // get time of day 
-//#include <time.h>
 #include "mymalloc.h"
-
-
-#define SUCCESS 0 
-#define FAILURE -1
-#define RUNS 50
 
 //gcc memgrind.c mymalloc.c -o memtest
 
 int task1();
 int task2();
 int task3();
-//int gettimeofday();
-
-double avg;
-double total;
-double iter;
-
-
 
 int main(int argc, char *argv[])
 {
-    struct timeval start, end;
-    // long seconds, micros;
-
-    gettimeofday(&start, NULL); // start timer. null value for tz bc we're not using it
-    
-    for(int i = 0; i < RUNS; i++) // execute workload 50 times
+    int t1, t2, t3;
+    t1 = task1();
+    if(t1 == 0)
     {
-        if(task1() == SUCCESS)
-        {
-             printf("Task 1 completed successfully\n");
-        }
-        else
-        {
-            printf("Task 1 failed\n");
-        }
-        
-        if(task2() == SUCCESS)
-        {
-            printf("Task 2 completed successfully\n");
-        }
-        else
-        {
-            printf("Task 2 failed\n");
-        }
-        
-        if(task3() == SUCCESS)
-        {
-            printf("Task 3 completed successfully\n");
-        }
-        else
-        {
-            printf("Task 3 failed\n");
-        }
+        printf("Task 1 completed successfully\n");
     }
-
-    gettimeofday(&end, NULL); // end timer
-    long seconds = end.tv_sec - start.tv_sec;
-    long micros = end.tv_usec - start.tv_usec;
-    total = (double)seconds + (micros / 1000000.0);
-    double avg_time = total / RUNS; // convert to milliseconds
-    return printf("The average time for performing the workload was %f seconds.", avg_time);
+    else
+    {
+        printf("Task 1 failed\n");
+    }
+    t2 = task2();
+    if(t2 == 0)
+    {
+        printf("Task 2 completed successfully\n");
+    }
+    else
+    {
+        printf("Task 2 failed\n");
+    }
+    t3 = task3();   
+    if(t3 == 0)
+    {
+        printf("Task 3 completed successfully\n");
+    }
+    else
+    {
+        printf("Task 3 failed\n");
+    }
+    return 0;
 }
 
 int task1()
@@ -74,12 +48,12 @@ int task1()
         char *ptr = malloc(1);
         if(ptr == NULL)
         {
-            return FAILURE;
+            return -1;
         }
         free(ptr);
         // printf("task 1: success. iteration %d\n", i);
     }
-    return SUCCESS;
+    return 0;
 }
 
 int task2() //Use malloc() to get 120 1-byte objects, storing the pointers in an array, then use free() to deallocate the chunks.
@@ -90,7 +64,7 @@ int task2() //Use malloc() to get 120 1-byte objects, storing the pointers in an
         ptr[i] = malloc(1);
         if (ptr[i] == NULL)
         {
-            return FAILURE;
+            return -1;
         }
         // printf("task 2: allocation success. iteration %d\n", i);
     }
@@ -99,7 +73,7 @@ int task2() //Use malloc() to get 120 1-byte objects, storing the pointers in an
         free(ptr[i]);
         // printf("task 2: deallocation success. iteration %d\n", i);
     }
-    return SUCCESS;
+    return 0;
 }
 
 int task3() //Create an array of 120 pointers. Repeatedly make a random choice between (a) allocating a 1-byte object and adding the pointer to the array and (b) deallocating a previously allocated object (if any). Once 120 allocations have been performed, deallocate all objects.
@@ -113,10 +87,6 @@ int task3() //Create an array of 120 pointers. Repeatedly make a random choice b
         if (choice == 0)
         {
             ptr[count] = malloc(1);
-            if(ptr[count] == NULL)
-            {
-                return FAILURE;
-            }
             count++;
             allocCount++;
         }
@@ -133,5 +103,5 @@ int task3() //Create an array of 120 pointers. Repeatedly make a random choice b
     {
         free(ptr[i]);
     }
-    return SUCCESS;
+    return 0;
 }
