5dfe7906fb603182080003fe50f904183bba1f50
Andrew <42828962+Lepieu@users.noreply.github.com>
2025-03-14 17:48:55 -0400

    Update outlier.c

--- Project2/outlier.c
+++ Project2/outlier.c
@@ -1,1 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+
+#define BUFFLENGTH 48
+
+char InvalidStart[] = { '(', '{', '[', '\"', '\'', };
+char InvalidEnd[] = { ')', '}', ']', '\"', '\'', '?', ',', '.', '!' }; 
+
+
+typedef struct Node {
+
+    char *word;
+    struct Node *next;
+    int freq;
+    char *fileName;
+
+} Node;
+
+
+
+
+int main() {
+
+    Node *front = NULL;
+
+
+
+
+    return 0;
+}
+
+// Loops through entire word linked list to find if the word already exists. If yes, return it and increase frequency. Otherwise, return NULL (so we can create the node)
+Node *FindWord(char *word, Node *ptr) {
+
+    while(ptr != NULL) {
+        if(strcmp(word, ptr->word) == 0) {
+            ptr->freq++;
+            return ptr;
+        }
+        ptr = ptr->next;
+    }
+    return NULL;
+
+}
+
+
+Node *CreateNode(char *word, Node *front) {
+
+    Node *newNode = malloc(sizeof(Node));
+    newNode->freq = 1;
+    newNode->word = word;
+
+    if(front == NULL) {
+        return newNode;
+    }
+    else {
+        newNode->next = front;
+        return newNode;
+    }
+
+}
