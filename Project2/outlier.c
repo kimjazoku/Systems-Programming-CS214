c7c44e6bb2b7b521264d36ce77ac1580648d4c64
Andrew <42828962+Lepieu@users.noreply.github.com>
2025-03-14 17:56:25 -0400

    Update outlier.c

--- Project2/outlier.c
+++ Project2/outlier.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 
-#define BUFFLENGTH 48
+#define BUFLEN 48
+#define DEBUG 0
 
 char InvalidStart[] = { '(', '{', '[', '\"', '\'', };
 char InvalidEnd[] = { ')', '}', ']', '\"', '\'', '?', ',', '.', '!' }; 
@@ -20,18 +22,6 @@ typedef struct Node {
 
 } Node;
 
-
-
-
-int main() {
-
-    Node *front = NULL;
-
-
-
-
-    return 0;
-}
 
 // Loops through entire word linked list to find if the word already exists. If yes, return it and increase frequency. Otherwise, return NULL (so we can create the node)
 Node *FindWord(char *word, Node *ptr) {
@@ -62,4 +52,28 @@ Node *CreateNode(char *word, Node *front) {
         return newNode;
     }
 
+}
+
+void WordReader(char *filename) {
+
+    char buf[BUFLEN];
+    int spaces = 0;
+
+//open and read file
+    int file = open(filename, O_RDONLY);
+    if(file == -1) {
+        perror("File not found");
+    }
+    int bytes = read(file, buf, BUFLEN);
+
+}
+
+int main(int argc, char *argv[]) {
+
+    Node *front = NULL;
+
+    
+
+
+    return 0;
 }
