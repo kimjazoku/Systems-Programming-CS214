85d7f000b43d93bcaa30d9d29c96b5a8ab9ac980
kimjazoku <jakegykim@gmail.com>
2025-03-17 21:23:18 -0400

    Update outlier.c

--- Project2/outlier.c
+++ Project2/outlier.c
@@ -59,18 +59,73 @@ void WordReader(char *filename) {
     char buf[BUFLEN];
     int spaces = 0;
 
-//open and read file
+    // open file
     int file = open(filename, O_RDONLY);
     if(file == -1) {
         perror("File not found");
     }
+    // read file
     int bytes = read(file, buf, BUFLEN);
+    if(bytes < 0) {
+        perror("Error reading file");
+        close(file);
+    }
+    else if(bytes == 0) {
+        puts("end of file");
+        close(file);
+    }
+    else {
+        buf[bytes] = '\0'; // add in the terminator
+        printf("%s\n", buf); // print out the buffer
+    }
+    
+    while(bytes > 0) {
+        for(int i = 0; i < bytes; i++) {
+            if(isspace(buf[i])) {
+                spaces++;
+            }
+        }
+        bytes = read(file, buf, BUFLEN);
+        printf("%s\n", buf);
+    }
 
+}
+
+int wordCounter(char *filename)
+{
+    // not sure if this is the best way to do this but it works
+    // we don't need a separate function for this, but it makes the main function cleaner
+    int words = 0;
+    int inWord = 0;
+    int bytes;
+    char buf[BUFLEN];
+    int file = open(filename, O_RDONLY);
+    if(file == -1) {
+        perror("File not found");
+    }
+    while((bytes = read(file, buf, BUFLEN)) > 0) {
+        for(int i = 0; i < bytes; i++) {
+            if(isspace(buf[i])) {
+                inWord = 0;
+            }
+            else if(inWord == 0) {
+                inWord = 1;
+                words++;
+            }
+        }
+    }
+    printf("Word count: %d\n", words);
+    if(bytes < 0) {
+        fprintf(stderr, "Error reading file %s\n", filename);
+    }
+    return words; // we can use this to keep track of how many words are in the file
 }
 
 int main(int argc, char *argv[]) {
 
     Node *front = NULL;
+    int wordCount = wordCounter(argv[1]); // used to get the word count
+    printf("Word count: %d\n", wordCount);
 
     
 
