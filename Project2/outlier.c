6b89dac7c8bb74f77883e4fc39464f91172d3db3
kimjazoku <jakegykim@gmail.com>
2025-03-23 00:19:44 -0400

    Update outlier.c

--- Project2/outlier.c
+++ Project2/outlier.c
@@ -9,14 +9,14 @@
 
 
 #define BUFLEN 48
-#define DEBUG 1
+#define DEBUG 0
 
 char InvalidStart[] = { '(', '{', '[', '\"', '\'', };
 char InvalidEnd[] = { ')', '}', ']', '\"', '\'', '?', ',', '.', '!' }; 
 
 
 typedef struct Node {
-
+    
     char *word;
     struct Node *next;
     int freq;
@@ -24,16 +24,18 @@ typedef struct Node {
 } Node;
 
 typedef struct FileNode {
-
+    
     Node *front;
     char *fileName;
     char *maxWord;
     int totalWords;
     float gri;
-
-
+    
+    
 } FileNode;
 
+FileNode *allFiles[100];
+int fileCount = 0;
 
 // Loops through entire word linked list to find if the word already exists. If yes, return it and increase frequency. Otherwise, return NULL (so we can create the node)
 Node *FindWord(char *word, Node *ptr) {
@@ -56,7 +58,7 @@ Node *CreateNode(char *word, Node *front) {
     newNode->word = word;
 
     if(DEBUG) {
-        //printf("%s ", newNode->word);
+        // printf("%s ", newNode->word);
     }
     newNode->next = front;
     return newNode;
@@ -125,17 +127,17 @@ Node *wordCounter(char *filename, FileNode *fileName)
     while((bytes = read(file, buf, BUFLEN)) > 0) {
         for(int i = 0; i < bytes; i++) {
             
-        //treat "Hello" == "hello"
+        // treat "Hello" == "hello"
             buf[i] = tolower(buf[i]);
 
 
-        //end of a word
+        // end of a word
             if(isspace(buf[i])) {
                 
                 inWord = 0;
                 currentWord[j] = '\0';
 
-            //loop through the word from the end. If there is an invalid character at the end, it will keep removing subsequent invalid characters until it finds a valid one
+            // loop through the word from the end. If there is an invalid character at the end, it will keep removing subsequent invalid characters until it finds a valid one
                 int validEnd = 1;
                 for(int w = strlen(currentWord) - 1; w >= 0; w--) {
                     for(int c = 0; c < sizeof(InvalidEnd); c++) {
@@ -146,10 +148,11 @@ Node *wordCounter(char *filename, FileNode *fileName)
                     }
                     if(validEnd) {
                         break;
-                    }   
+                    }
+                    validEnd = 1;
                 }
 
-            //loop through linked list to find if the word was already found in the file
+            // loop through linked list to find if the word was already found in the file
                 Node *currentNode = FindWord(currentWord, front);
                 if(currentNode == NULL) {
 
@@ -164,7 +167,7 @@ Node *wordCounter(char *filename, FileNode *fileName)
               
             }
 
-        //start of a word
+        // start of a word
             else if(inWord == 0) {
                 
                 int validStart = 1;
@@ -183,7 +186,7 @@ Node *wordCounter(char *filename, FileNode *fileName)
 
             }
 
-        //in word
+        // in word
             if(inWord == 1) {
                currentWord[j] = buf[i];
                j++; 
@@ -191,6 +194,7 @@ Node *wordCounter(char *filename, FileNode *fileName)
         }
     }
 
+    // end of file
     if(inWord == 1 && j > 0) {
         
         inWord = 0;
@@ -206,14 +210,15 @@ Node *wordCounter(char *filename, FileNode *fileName)
             }
             if(validEnd) {
                 break;
-            }   
+            }
+            validEnd = 1;   
         }
 
-    //loop through linked list to find if the word was already found in the file
+    // loop through linked list to find if the word was already found in the file
         Node *currentNode = FindWord(strdup(currentWord), front);
         if(currentNode == NULL) {
 
-        //if it doesn't exist, add it to the front
+        // if it doesn't exist, add it to the front
             front = CreateNode(strdup(currentWord), front);
         }
         else {
@@ -231,7 +236,7 @@ Node *wordCounter(char *filename, FileNode *fileName)
     return front; // we can use this to keep track of how many words are in the file
 }
 
-void PrintTable(FileNode *arr, int size) {
+void PrintTable(FileNode **arr, int size) {
 
     printf("\tWord \t | \t");
     
@@ -239,17 +244,17 @@ void PrintTable(FileNode *arr, int size) {
     int totalWords = 0;
 
     for(int i = 0; i < size; i++) {
-        printf("%s \t | \t", (*(arr+i)).fileName);
+        printf("%s \t | \t", (*(arr+i))->fileName);
 
-        Node *ptr = arr[i].front;
+        Node *ptr = arr[i]->front;
         while(ptr != NULL) {
             
-        //See if current word is in AllWords. If not, add it.
+        // See if current word is in AllWords. If not, add it.
             Node *currentNode = FindWord(ptr->word, allWords);
             if(currentNode == NULL) {
                 allWords = CreateNode(ptr->word, allWords);
             }
-        //if it is, increase TOTAL frequency  
+        // if it is, increase TOTAL frequency  
             else {
                 currentNode->freq += ptr->freq;
             }
@@ -271,8 +276,8 @@ void PrintTable(FileNode *arr, int size) {
 
         for(int i = 0; i < size; i++) {
         
-        //finds amount of times word appears in file arr[i](should be under the correct letter)
-            Node *wordForFile = FindWord(ptr->word, arr[i].front);
+        // finds amount of times word appears in file arr[i](should be under the correct letter)
+            Node *wordForFile = FindWord(ptr->word, arr[i]->front);
 
             if(wordForFile == NULL) {
 
@@ -280,7 +285,7 @@ void PrintTable(FileNode *arr, int size) {
                 
             }
             else {
-                float freakyFile = (float) wordForFile->freq / (float) arr[i].totalWords;
+                float freakyFile = (float) wordForFile->freq / (float) arr[i]->totalWords;
                 printf("%.2f \t | \t", freakyFile);
             }
 
@@ -308,14 +313,14 @@ void PrintTable(FileNode *arr, int size) {
     while(temp != NULL) {
 
         for(int i = 0; i < size; i++) {
-            Node *wordForFile = FindWord(temp->word, arr[i].front);
+            Node *wordForFile = FindWord(temp->word, arr[i]->front);
             
             if(wordForFile != NULL) {
                 // calculate overall freq for the word
                 float overall = (float) temp->freq / (float) totalWords;
 
                 // calculate the word's freq in the file
-                float freakyFile = (float) wordForFile->freq / (float) arr[i].totalWords;
+                float freakyFile = (float) wordForFile->freq / (float) arr[i]->totalWords;
 
                 // calculate the word's freq relative to overall freq
                 float wordFreq = freakyFile / overall;
@@ -330,7 +335,7 @@ void PrintTable(FileNode *arr, int size) {
                     // maxFile[i]->gri = wordFreq; // store the word's relative increase
                     // maxFile[i]->maxWord = temp->word; // store the word
                 }
-                maxFile[i]->fileName = arr[i].fileName; // store the file
+                maxFile[i]->fileName = arr[i]->fileName; // store the file
                 if(wordFreq > maxFile[i]->gri)
                 {
                     maxFile[i]->gri = wordFreq; // store the word's relative increase
@@ -339,9 +344,9 @@ void PrintTable(FileNode *arr, int size) {
  
                 if(DEBUG)
                 {
-                   //printf("%s: %s == %f\n", arr[i].fileName, wordForFile->word, wordFreq);
+                   // printf("%s: %s == %f\n", arr[i].fileName, wordForFile->word, wordFreq);
                 }
-                //    printf("%s: %s\n", arr[i].fileName, temp->word);
+                // printf("%s: %s\n", arr[i].fileName, temp->word);
             }
         }
         temp = temp->next;
@@ -362,74 +367,103 @@ void PrintTable(FileNode *arr, int size) {
 }
 
 
-void DirectoryTraversal(struct stat *fileInfo, char *fileName, FileNode *file) {
+int isTxtFile(char *fileName) {
+    char *extension = ".txt";
+    size_t extSize = strlen(extension);
+    size_t fileNameSize = strlen(fileName);
+
+    if(fileNameSize < extSize) {
+        return 0;
+    }
+
+    return strcmp(fileName + fileNameSize - extSize, extension) == 0;
+}
+
+
+void DirectoryTraversal(struct stat *fileInfo, char *fileName) {
     
     char *extension = ".txt";
     size_t extSize = strlen(extension);
 
     
-
-    if(stat(fileName, fileInfo) == 0) {
-        if(S_ISREG(fileInfo->st_mode)) {
-            if(DEBUG) {
-                // printf("Regular File\n");
+    if (stat(fileName, fileInfo) == 0) {
+            if (S_ISREG(fileInfo->st_mode)) {
+                if (isTxtFile(fileName)) { 
+                    allFiles[fileCount] = malloc(sizeof(FileNode));
+                    allFiles[fileCount]->fileName  = strdup(fileName);
+                    allFiles[fileCount]->front     = NULL;
+                    allFiles[fileCount]->maxWord   = NULL;
+                    allFiles[fileCount]->totalWords= 0;
+                    allFiles[fileCount]->gri       = 0.0f;
+                    allFiles[fileCount]->front = wordCounter(fileName, allFiles[fileCount]);
+                    fileCount++;
+                }
             }
-            
-            if(strncmp(fileName + strlen(fileName) - extSize, extension, extSize) == 0) {
+
+            else if(S_ISDIR(fileInfo->st_mode)) {
+
+                
                 if(DEBUG) {
-                    // printf("Text File\n");
+                    // printf("Directory\n");
                 }
                 
-                file->front = wordCounter(fileName, file);
-            }
-
-        }
-
-        else if(S_ISDIR(fileInfo->st_mode)) {
-
-            
-            if(DEBUG) {
-                // printf("Directory\n");
-            }
-            
-            struct dirent *dEnt;
-            
-            DIR *directory = opendir(fileName);
-            
-            if (directory == NULL) {
-                perror("Could not open directory");
-                return;
-            }
-            while((dEnt = readdir(directory)) != NULL) {
+                struct dirent *dEnt;
                 
-                if(strcmp(dEnt->d_name, ".") != 0 && strcmp(dEnt->d_name, "..") != 0) {
+                DIR *directory = opendir(fileName);
+                
+                if (directory == NULL) {
+                    perror("Could not open directory");
+                    return;
+                }
+                while((dEnt = readdir(directory)) != NULL) {
                     
-                    char newPath[1000];
-                    
-                    if(DEBUG) {
-                        printf("%s\n", dEnt->d_name);
+                    if(strcmp(dEnt->d_name, ".") != 0 && strcmp(dEnt->d_name, "..") != 0) {
+                        
+                        char newPath[1000];
+                        
+                        if(DEBUG) {
+                            printf("%s\n", dEnt->d_name);
+                        }
+                        
+                        strcpy(newPath, fileName);
+                        strcat(newPath, "/");
+                        strcat(newPath, dEnt->d_name);
+
+                        DirectoryTraversal(fileInfo, newPath);
+
                     }
-                    
-                    strcpy(newPath, fileName);
-                    strcat(newPath, "/");
-                    strcat(newPath, dEnt->d_name);
-
-                    DirectoryTraversal(fileInfo, newPath, file);
+                    char *newPath;
 
                 }
-                char *newPath;
-
+                closedir(directory);
             }
-            closedir(directory);
+            else {
+                if(DEBUG) {
+                    printf("The fuck wrong witchu");
+                }
+            }
         }
         else {
-            if(DEBUG) {
-                printf("The fuck wrong witchu");
-            }
+            perror("sum wrong wit the file gang");
         }
-    }
-    else {
-        perror("sum wrong wit the file gang");
+}
+
+void freeAllFiles(FileNode **files, int count) {
+    for (int i = 0; i < count; i++) {
+        if (files[i] != NULL) {
+            // free the list 
+            Node *curr = files[i]->front;
+            while (curr != NULL) {
+                Node *temp = curr;
+                curr = curr->next;
+                free(temp->word);  
+                free(temp);        
+            }
+            
+            free(files[i]->fileName);
+            
+            free(files[i]);
+        }
     }
 }
 
@@ -445,44 +479,31 @@ int main(int argc, char *argv[]) {
         // argv[3] = "abc/c.txt";
     }
     
-    FileNode *files = malloc((argc - 1) * sizeof(FileNode));
-    
-    
-    for(int i = 1; i < argc; i++)
-    {
-        
-        files[i - 1].front = NULL;
-        files[i - 1].fileName = argv[i];
-        
+    for (int i = 1; i < argc; i++) {
         struct stat fileInfo;
-        DirectoryTraversal(&fileInfo, files[i-1].fileName, &files[i - 1]);
-        
-        //files[i - 1].front = wordCounter(argv[i], &files[i-1]); // used to get the word count
+        if (stat(argv[i], &fileInfo) == 0) {
+            if (S_ISDIR(fileInfo.st_mode)) {
+                DirectoryTraversal(&fileInfo, argv[i]);
+            } else if (S_ISREG(fileInfo.st_mode)) {
+                if (isTxtFile(argv[i])) {
+                    allFiles[fileCount] = malloc(sizeof(FileNode));
+                    // initialize
+                    allFiles[fileCount]->fileName  = strdup(argv[i]);
+                    allFiles[fileCount]->front     = NULL;
+                    allFiles[fileCount]->maxWord   = NULL;
+                    allFiles[fileCount]->totalWords= 0;
+                    allFiles[fileCount]->gri       = 0.0f;
 
-
-
-        if(DEBUG) {
-
-            printf("File: %s\n", files[i-1].fileName);
-            printf("total words in file: %d\n", files[i-1].totalWords);
-
-            Node *ptr = files[i-1].front;
-            while(ptr != NULL) {
-                printf("%s ", ptr->word);
-                printf("(%d) ", ptr->freq);
-                ptr = ptr->next;
+                    allFiles[fileCount]->front = wordCounter(argv[i], allFiles[fileCount]);
+                    fileCount++;
+                }
             }
-            printf("\n\n");
-        } 
+        }
     }
     
-    PrintTable(files, argc - 1);
+    PrintTable(allFiles, fileCount);
 
-    for(int i = 0; i < argc - 1; i++) {
-
-        //free(&files[i]);
-    }
-    //free(files);
+    freeAllFiles(allFiles, fileCount);
 
     return 0;
 }
