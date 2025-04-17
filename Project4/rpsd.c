5da39d796461e8ecab3b05122c5a7bce5416af4f
kimjazoku <jakegykim@gmail.com>
2025-04-17 17:53:11 -0400

    Update rpsd.c

--- Project4/rpsd.c
+++ Project4/rpsd.c
@@ -1,1 +1,51 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+
+#include "connection.h"
+
+
+// messages sent by client
+void play() // Argument: player’s name. 
+            // Sent by client after establishing a connection.
+{
+
+}
+
+void move() //Argument: one of ROCK, PAPER, SCISSORS. Sent by 
+            // client after receiving Begin from server.
+{
+
+}
+
+void cont()
+{
+
+}
+
+void quit()
+{
+
+}
+
+// messages sent by server
+void wait()
+{
+
+}
+
+void begin()
+{
+
+}
+
+void result()
+{
+    char result[] = {"W", "L", "D", "F"}; // Win, Lose, Draw, Forfeit
+}
 
